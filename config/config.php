6e6a7aca4f3601e6d812470b2f69f817b6233230
Ajay Ramsaran <ajayramsaran2012@gmail.com>
2026-01-24 00:46:19 -0500

    credentials and sessions

---
+++ config/config.php
@@ -0,0 +1,10 @@
+<?php
+//DB credentials, constants, and global settings
+// config/config.php
+
+$DB_HOST = "localhost";
+$DB_NAME = "citadel";
+$DB_USER = "root";
+$DB_PASS = "your_db_password_here"; //password
+$API_KEY = "your_api_key_here"; //API key for google maps
+?>
