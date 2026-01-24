11d64b9fa959bc5d5624e4caaf6578efd1eed5c9
Ajay Ramsaran <ajayramsaran2012@gmail.com>
2026-01-24 18:32:09 -0500

    updated with Python path

--- config/config.php
+++ config/config.php
@@ -5,6 +5,7 @@
 $DB_HOST = "localhost";
 $DB_NAME = "citadel";
 $DB_USER = "root";
-$DB_PASS = "your_db_password_here"; //password
-$API_KEY = "your_api_key_here"; //API key for google maps
+$DB_PASS = ""; //password
+$API_KEY = ""; //API key for google maps
+$PYTHON_PATH='C:\Users\ajayr\AppData\Local\Programs\Python\Python314\python.exe'
 ?>
