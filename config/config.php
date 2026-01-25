49876d71e48ce0e9a72b1c62a243ae586cb8add3
Sameer <sameerkarodia@gmail.com>
2026-01-25 06:38:45 -0500

    new readme

--- config/config.php
+++ config/config.php
@@ -5,7 +5,7 @@
 $DB_HOST = "localhost";
 $DB_NAME = "citadel";
 $DB_USER = "root";
-$DB_PASS = "Malvern"; //password
+$DB_PASS = ""; //password
 $API_KEY = ""; //API key for google maps
 $PYTHON_PATH='C:\Users\samee\AppData\Local\Programs\Python\Python313\python.exe'
 ?>
